6861e7d6851f3a147535926aed8784e4528556d6
neymarssj <143804583+neymarssj@users.noreply.github.com>
2024-05-20 09:05:52 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
   
 #!/data/data/com.termux/files/usr/bin/bash  
 BOT_DIR="The-LoliBot-MD"  
-BOT_REPO="https://github.com/elrebelde21/$BOT_DIR" 
+BOT_REPO="https://github.com/neymarssj/$BOT_DIR" 
 DB_FILE="database.json"
   
 GREEN='\033[32m' 
